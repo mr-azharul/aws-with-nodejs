d2fd86372380e7134f84e4d3afa198fe031e16b6
Azharul Islam <azharul.sylhet@gmail.com>
2023-02-14 00:18:32 +0600

    s3 bucket

--- src/controllers/s3.controllers.ts
+++ src/controllers/s3.controllers.ts
@@ -1,3 +1,11 @@
+import AWS from "aws-sdk";
+const S3 = new AWS.S3({
+    region: "us-east-1",
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET
+});
+
 export const getAllBucket = (req:any, res:any) => {
-    res.send("all bucket list");
+    const list = S3.listBuckets();
+    res.send(list);
 }
